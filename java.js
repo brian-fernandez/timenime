2b6a6b74e176699337e3a0271a72cd6adfeab0b4
brian <nsklobuchar666@gmail.com>
2020-09-27 11:23:09 -0400

    asdf

---
+++ java.js
@@ -0,0 +1,21 @@
+
+
+function activacion()
+{
+    const act = document.querySelector('.barra1');
+    const act2 = document.querySelector('.barra2');
+    act.classList.toggle("active");
+   
+    
+
+};
+function activacion2()
+{
+    
+    const act2 = document.querySelector('.barra2');
+    
+   
+    act2.classList.toggle("active2");
+
+}
+
